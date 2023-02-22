ab2c03192e1fc1ca8debb1e9e96714b3fe48acbf
Samir Kattel <samirdon963@gmail.com>
2023-02-22 22:18:36 +0545

    Fixing build errors

--- src/features/user/userView.tsx
+++ src/features/user/userView.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { fetchUsers } from "./userSlice";
 
